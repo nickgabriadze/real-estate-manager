df3897b0414317b6823873de654b9b09b9766d49
Nick <nickgabriadze@gmail.com>
2024-09-12 19:45:13 +0400

    feat: implement add agent component

---
+++ src/pages/create-agent/AddAgent.tsx
@@ -0,0 +1,28 @@
+import addAgentStyles from './addagent.module.css'
+import UploadPicture from "../../global-components/UploadPicture.tsx";
+import Input from "../../global-components/Input.tsx";
+import addListingStyles from "../create-listing/addlisting.module.css";
+
+
+export default function AddAgent() {
+
+    return <section className={addAgentStyles['agentFormWrapper']}>
+            <h1>აგენტის დამატება</h1>
+
+        <form>
+            <div className={addAgentStyles['inputInformation']}>
+                <Input label={"სახელი"} validator={"მინიმუმ ორი სიმბოლო"} type={"text"} required={true}/>
+                <Input label={"გვარი"} validator={"მინიმუმ ორი სიმბოლო"} type={"text"} required={false}/>
+                <Input label={"ელ-ფოსტა"} validator={"გამოიყენეთ @redberry.ge ფოსტა"} type={"email"} required={true}/>
+                <Input label={"ტელეფონის ნომერი"} validator={"მხოლოდ რიცხვები"} type={"number"} required={true}/>
+
+            </div>
+            <UploadPicture label={'ატვირთეთ ფოტო'} required={true}/>
+
+            <div className={addListingStyles['formButtons']}>
+                <button type={'reset'}>გაუქმება</button>
+                <button type={'submit'}>დაამატე აგენტი</button>
+            </div>
+        </form>
+    </section>
+}
