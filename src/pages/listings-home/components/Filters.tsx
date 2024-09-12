ff73a47c32e456fb4ec91385c0608a63722c8984
Nick <nickgabriadze@gmail.com>
2024-09-12 18:47:39 +0400

    feat: update filter component with new handler

--- src/pages/listings-home/components/Filters.tsx
+++ src/pages/listings-home/components/Filters.tsx
@@ -3,19 +3,32 @@ import Region from "./filter-options/Region.tsx";
 import Pricing from "./filter-options/Pricing.tsx";
 import Area from "./filter-options/Area.tsx";
 import Rooms from "./filter-options/Rooms.tsx";
+import AddIconWhiteSVG from '/src-icons/add-white.svg'
+import AddIconOrangeSVG from '/src-icons/add-orange.svg'
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 
 export default function Filters() {
+    const [optionOpen, setOptionOpen] = useState<
+        "region" | "pricing" | "area" | "rooms" | "none"
+    >("none")
 
-    return <section>
+
+    return <section className={filterStyles['filterWrapper']}>
         <div className={filterStyles['filtersCreate']}>
             <div className={filterStyles['filterOptions']}>
-                <Region/>
-                <Pricing/>
+                <Region visible={{status: optionOpen === 'region', makeVisible: setOptionOpen}}/>
+                <Pricing />
                 <Area/>
-                <Rooms/>
+                <Rooms visible={{status: optionOpen === 'rooms', makeVisible: setOptionOpen}}/>
             </div>
 
+            <div className={filterStyles['create']}>
+                <Link to={'/add-listing'}><img src={AddIconWhiteSVG} width={12} alt={'Add icon'}/><h4>ლისტინგის დამატება</h4></Link>
+                <button><img src={AddIconOrangeSVG} width={12} alt={'Add icon'}/><h4>აგენტის დამატება</h4></button>
+
+            </div>
         </div>
 
         <div className={filterStyles['selectedFilters']}>
