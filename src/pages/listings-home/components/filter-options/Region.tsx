3382bc33f7288f167ce8e1012b6a66be30a41580
Nick <nickgabriadze@gmail.com>
2024-09-12 18:47:19 +0400

    feat: add handling dropdown functionality

--- src/pages/listings-home/components/filter-options/Region.tsx
+++ src/pages/listings-home/components/filter-options/Region.tsx
@@ -2,15 +2,19 @@ import {useQuery} from "@tanstack/react-query";
 import getRegions from "../../../../apis/getRegions.ts";
 import filterOptionsStyles from '../styles/filters.options.module.css'
 import filterStyles from '../styles/filters.module.css'
-import {useState} from "react";
+import React, {useState} from "react";
 import UpArrowSVG from '/src-icons/up-arrow.svg'
 import DownArrowSVG from '/src-icons/down-arrow.svg'
 import {useNavigate} from "react-router-dom";
 
-export default function Region() {
+export default function Region({visible}: {
+    visible: {
+        status: boolean,
+        makeVisible: React.Dispatch<React.SetStateAction<"region" | "pricing" | "area" | "rooms" | "none">>
+    }
+}) {
 
-    const {data, isLoading} = useQuery({queryKey: ['selectedRegions'], queryFn: getRegions,})
-    const [open, setOpen] = useState<boolean>(false);
+    const {data, isLoading} = useQuery({queryKey: ['regions'], queryFn: getRegions,})
     const regions = data?.data
     const navigate = useNavigate()
     const searchParams = new URLSearchParams(location.search)
@@ -33,25 +37,30 @@ export default function Region() {
 
     return (
         <div className={filterOptionsStyles['regionsWrapper']}>
-            <button onClick={() => setOpen(!open)}>
+            <button
+                className={filterOptionsStyles['filterOptionsButton']}
+                onClick={() => visible.makeVisible(visible.status ? 'none' : 'region')}>
                 <h3>რეგიონი</h3>
-                <img src={open ? UpArrowSVG : DownArrowSVG} alt={'Arrow icon'} width={24}/>
+                <img src={visible.status ? UpArrowSVG : DownArrowSVG} alt={'Arrow icon'} width={12}/>
             </button>
 
-            {open && regions && !isLoading &&
+            {visible.status && regions && !isLoading &&
                 <div className={filterOptionsStyles['regions']}>
                     <h3>რეგიონის მიხედვით</h3>
                     <div className={filterOptionsStyles['mappedRegions']}>{regions.map((region) => <div
-                            key={region.id}
-                            className={filterOptionsStyles['eachRegion']}>
-                            <input type={'checkbox'}
-                                   checked={selectedRegions.includes(region.id)}
-                                   onChange={() => handleRegion(region.id)}/>
-                            <p>{region.name}</p>
-                        </div>)}
+                        key={region.id}
+                        className={filterOptionsStyles['eachRegion']}>
+                        <input type={'checkbox'}
+                               checked={selectedRegions.includes(region.id)}
+                               onChange={() => handleRegion(region.id)}/>
+                        <p>{region.name}</p>
+                    </div>)}
                     </div>
                     <div>
-                        <button className={filterStyles['dropdownSelectButton']}>არჩევა</button>
+                        <button className={filterStyles['dropdownSelectButton']}
+                                onClick={() => visible.makeVisible(visible.status ? 'none' : 'region')}
+                        >არჩევა
+                        </button>
                     </div>
                 </div>
 
--- src/pages/listings-home/components/filter-options/Rooms.tsx
+++ src/pages/listings-home/components/filter-options/Rooms.tsx
@@ -1,6 +1,22 @@
+import filterOptionsStyles from '../styles/filters.options.module.css'
+import UpArrowSVG from "/src-icons/up-arrow.svg";
+import DownArrowSVG from "/src-icons/down-arrow.svg";
+import React from "react";
 
-export default function Rooms() {
+
+export default function Rooms({visible}: {
+    visible: {
+        status: boolean,
+        makeVisible: React.Dispatch<React.SetStateAction<"region" | "pricing" | "area" | "rooms" | "none">>
+    }
+}) {
+
     return (
-        <div></div>
+        <div className={filterOptionsStyles['roomsWrapper']}>
+            <button className={filterOptionsStyles['filterOptionsButton']} onClick={() => visible.makeVisible(visible.status ? 'none' : 'rooms')}>
+                <h3>ოთახების რაოდენობა</h3>
+                <img src={visible.status ? UpArrowSVG : DownArrowSVG} alt={'Arrow icon'} width={12}/>
+            </button>
+        </div>
     );
 }
