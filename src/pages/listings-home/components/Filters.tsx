673ec024e21d691473b85fbe7662fd6e48d1558d
Nick <nickgabriadze@gmail.com>
2024-09-12 15:16:54 +0400

    feat: add filters component

--- src/pages/listings-home/components/Filters.tsx
+++ src/pages/listings-home/components/Filters.tsx
@@ -1,4 +1,26 @@
-export default function Filters(){
+import filterStyles from './styles/filters.module.css'
+import Region from "./filter-options/Region.tsx";
+import Pricing from "./filter-options/Pricing.tsx";
+import Area from "./filter-options/Area.tsx";
+import Rooms from "./filter-options/Rooms.tsx";
 
-    return <section></section>
+
+export default function Filters() {
+
+    return <section>
+        <div className={filterStyles['filtersCreate']}>
+            <div className={filterStyles['filterOptions']}>
+                <Region/>
+                <Pricing/>
+                <Area/>
+                <Rooms/>
+            </div>
+
+        </div>
+
+        <div className={filterStyles['selectedFilters']}>
+
+
+        </div>
+    </section>
 }
