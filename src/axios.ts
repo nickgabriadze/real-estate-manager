cd547474a07dc7b1773dc5ebc50f7a574a2067de
Nick <nickgabriadze@gmail.com>
2024-09-12 13:24:24 +0400

    refactor: update axios instance base_url

--- src/axios.ts
+++ src/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-    baseURL: 'api.real-estate-manager.redberryinternship.ge',
+    baseURL: 'api.real-estate-manager.redberryinternship.ge/api',
     headers: {
         Authorization: `Bearer ${import.meta.env.API_ACCESS_TOKEN}`
     }
