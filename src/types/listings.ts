e7d972f42030c705afd6416e926aa876b42d76dd
Nick <nickgabriadze@gmail.com>
2024-09-13 01:33:11 +0400

    feat: add type for listings

---
+++ src/types/listings.ts
@@ -0,0 +1,17 @@
+import {City} from "./cities.ts";
+import {Region} from "./regions.ts";
+
+export type Listing = {
+    id: number,
+    address: string,
+    zip_code: string,
+    price: number,
+    area: number,
+    bedrooms: number,
+    is_rental: number,
+    image: string,
+    city_id: number,
+    city: City & { region: Region }
+}
+
+export type Listings = Listing[]
