087bec846f2fbd4d8a4ec731352ba98fdd5f4e32
Nick <nickgabriadze@gmail.com>
2024-09-12 19:43:51 +0400

    feat: adding blur and reduced brightness overlay to the entire page when 'add agent' component is open

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import RedBerrySVG from '/redberry-icon.svg'
 import appStyles from './app.module.css'
-import {Link, Route, Routes} from "react-router-dom";
+import {Link, Route, Routes, useLocation} from "react-router-dom";
 import Listings from "./pages/listings-home/Listings.tsx";
 import AddListing from './pages/create-listing/AddListing.tsx'
+import AddAgent from "./pages/create-agent/AddAgent.tsx";
+import addAgentStyles from "./pages/create-agent/addagent.module.css";
 
 export default function App() {
-
-
+    const location = useLocation()
     return <section className={appStyles['applicationWrapper']}>
 
         <header>
@@ -14,9 +15,12 @@ export default function App() {
         </header>
         <hr/>
 
+        {location.pathname === '/add-agent' && <div className={addAgentStyles['outline']}></div>}
         <Routes>
-            <Route index element={<Listings/>}/>
-            <Route path={'/add-listing'} element={<AddListing />} />
+            <Route path={'/'} element={<Listings/>}>
+                <Route path={'add-agent'} element={<AddAgent/>}/>
+            </Route>
+            <Route path={'/add-listing'} element={<AddListing/>}/>
         </Routes>
 
     </section>
