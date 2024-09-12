544a64126d9acd04af2750ceb6ee901905288edd
Nick <nickgabriadze@gmail.com>
2024-09-13 01:33:38 +0400

    refactor: add return type to function

--- src/apis/getListings.ts
+++ src/apis/getListings.ts
@@ -1,6 +1,8 @@
 import axiosInstance from "../axios.ts";
+import {AxiosResponse} from "axios";
+import {Listings} from "../types/listings.ts";
 
-export default async function getListings(){
+export default async function getListings():Promise<AxiosResponse<Listings[]>> {
 
     return await axiosInstance.get('/real-estates')
 }
