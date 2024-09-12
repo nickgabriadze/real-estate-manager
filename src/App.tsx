8a732decf98a4fba43a9350dbb4c6b7d58378ccd
Nick <nickgabriadze@gmail.com>
2024-09-12 13:25:07 +0400

    feat: add header and routes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import RedBerrySVG from '/redberry-icon.svg'
+import appStyles from './app.module.css'
+import {Route, Routes} from "react-router-dom";
+import Listings from "./pages/listings-home/Listings.tsx";
+
 export default function App(){
 
 
-    return <h1>Hello</h1>
+    return <section className={appStyles['application-wrapper']}>
+
+            <header>
+                <div><img src={RedBerrySVG} width={150} height={24} alt={'RedBerry icon'} /></div>
+            </header>
+
+        <Routes>
+            <Route path={'/listings'} element={<Listings />}/>
+        </Routes>
+
+    </section>
 }
 
