44f8f992937a6256a2720c5da205f7d1890ff0a8
Nick <nickgabriadze@gmail.com>
2024-09-12 18:48:37 +0400

    feat: add url for page that lets you create listing and attach homepage link to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import RedBerrySVG from '/redberry-icon.svg'
 import appStyles from './app.module.css'
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 import Listings from "./pages/listings-home/Listings.tsx";
+import AddListing from './pages/create-listing/AddListing.tsx'
 
-export default function App(){
+export default function App() {
 
 
     return <section className={appStyles['applicationWrapper']}>
 
-            <header>
-                <div><img src={RedBerrySVG} width={150} height={24} alt={'RedBerry icon'} /></div>
-            </header>
+        <header>
+            <Link to={'/'}><img src={RedBerrySVG} width={150} height={24} alt={'RedBerry icon'}/></Link>
+        </header>
+        <hr/>
 
         <Routes>
-            <Route index element={<Listings />}/>
+            <Route index element={<Listings/>}/>
+            <Route path={'/add-listing'} element={<AddListing />} />
         </Routes>
 
     </section>
