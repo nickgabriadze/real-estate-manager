722632e6791bfabdd0df97b8c071eea3c471fe9f
Nick <nickgabriadze@gmail.com>
2024-09-13 19:28:28 +0400

    fix: update message for 404 page

--- src/404.tsx
+++ src/404.tsx
@@ -5,7 +5,7 @@ export default function PageNotFound404() {
 
     return <div className='not-found'>
         <div>
-            <span>404</span> <span>|</span> <span> სამწუხაროდ, მოცემული გვერდი ვერ მოიძებნა</span>
+            <span>404</span> <span>|</span> <span> სამწუხაროდ, ინფორმაცია ვერ მოიძებნა</span>
         </div>
         <div>
             <Link to={'/'}>მთავარ გვერდზე
