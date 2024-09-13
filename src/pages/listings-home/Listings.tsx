6a66d094d50638630791b50f09f6f377fa9790c8
Nick <nickgabriadze@gmail.com>
2024-09-13 19:02:47 +0400

    feat: add Homes component to show available listings

--- src/pages/listings-home/Listings.tsx
+++ src/pages/listings-home/Listings.tsx
@@ -1,11 +1,13 @@
 import Filters from "./components/Filters.tsx";
 import {Outlet} from "react-router-dom";
+import Homes from "./components/homes/Homes.tsx";
 
-export default function Listings(){
+export default function Listings() {
 
     return <>
 
-        <Outlet />
-        <Filters />
+            <Outlet/>
+            <Filters/>
+            <Homes />
     </>
 }
