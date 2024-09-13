93f77031fda7e21d14bd10d9f09a8a520d9fc642
Nick <nickgabriadze@gmail.com>
2024-09-13 18:57:14 +0400

    feat: add api to retrieve all the available listings

--- src/apis/getListings.ts
+++ src/apis/getListings.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../axios.ts";
 import {AxiosResponse} from "axios";
 import {Listings} from "../types/listings.ts";
 
-export default async function getListings():Promise<AxiosResponse<Listings[]>> {
+export default async function getListings():Promise<AxiosResponse<Listings>> {
 
     return await axiosInstance.get('/real-estates')
 }
