b2f44cb11a36b9ff308caf8a670197a79d23d555
Nick <nickgabriadze@gmail.com>
2024-09-14 20:17:09 +0400

    fix: update listing type to add a few more properties

--- src/types/listings.ts
+++ src/types/listings.ts
@@ -1,5 +1,6 @@
 import {City} from "./cities.ts";
 import {Region} from "./regions.ts";
+import {Agent} from "./agents.ts";
 
 export type Listing = {
     id: number,
@@ -11,7 +12,12 @@ export type Listing = {
     is_rental: 0 | 1,
     image: string,
     city_id: number,
-    city: City & { region: Region }
+    created_at: string
+    city: City & { region: Region },
+    description: string,
+    agent_id :number,
+    agent: Agent
+
 }
 
 export type Listings = Listing[]
