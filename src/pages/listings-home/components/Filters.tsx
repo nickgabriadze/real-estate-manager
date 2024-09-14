8e3d2ce08fe5b0b3de73cdc0a6928176e6d733e9
Nick <nickgabriadze@gmail.com>
2024-09-14 14:25:34 +0400

    feat: fetching when the website loads(instead of after opening the 'region filter). added chosen filters under the filter options

--- src/pages/listings-home/components/Filters.tsx
+++ src/pages/listings-home/components/Filters.tsx
@@ -1,5 +1,5 @@
 import filterStyles from './styles/filters.module.css'
-import Region from "./filter-options/Region.tsx";
+import RegionFilter from "./filter-options/RegionFilter.tsx";
 import Pricing from "./filter-options/Pricing.tsx";
 import Area from "./filter-options/Area.tsx";
 import Rooms from "./filter-options/Rooms.tsx";
@@ -7,33 +7,52 @@ import AddIconWhiteSVG from '/src-icons/add-white.svg'
 import AddIconOrangeSVG from '/src-icons/add-orange.svg'
 import {useState} from "react";
 import {Link} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../../hooks/redux.ts";
+import {useQuery} from "@tanstack/react-query";
+import getRegions from "../../../apis/getRegions.ts";
+import RemoveSVG from '/src-icons/remove.svg'
+import {removeRegionFilters, resetAll} from "../../../features/filters/filterReducer.ts";
 
 
 export default function Filters() {
     const [optionOpen, setOptionOpen] = useState<
         "region" | "pricing" | "area" | "rooms" | "none"
     >("none")
+    const filterDispatch = useAppDispatch()
+    const {data, isLoading} = useQuery({queryKey: ['regions'], queryFn: getRegions,})
 
+    const filters = useAppSelector(s => s.filters)
+    const filtersAvailable = filters.regionFilters.length + filters.areaFilter.length + filters.priceFilter.length + filters.roomFilters.length !== 0
 
     return <section className={filterStyles['filterWrapper']}>
         <div className={filterStyles['filtersCreate']}>
             <div className={filterStyles['filterOptions']}>
-                <Region visible={{status: optionOpen === 'region', makeVisible: setOptionOpen}}/>
-                <Pricing />
+                <RegionFilter regionsData={data?.data ? data.data : []} isLoading={isLoading}
+                              visible={{status: optionOpen === 'region', makeVisible: setOptionOpen}}/>
+                <Pricing/>
                 <Area/>
                 <Rooms visible={{status: optionOpen === 'rooms', makeVisible: setOptionOpen}}/>
             </div>
 
             <div className={filterStyles['create']}>
-                <Link to={'/add-listing'}><img src={AddIconWhiteSVG} width={12} alt={'Add icon'}/><h4>ლისტინგის დამატება</h4></Link>
-                <Link to={'/add-agent'}><img src={AddIconOrangeSVG} width={12} alt={'Add icon'}/><h4>აგენტის დამატება</h4></Link>
+                <Link to={'/add-listing'}><img src={AddIconWhiteSVG} width={12} alt={'Add icon'}/><h4>ლისტინგის
+                    დამატება</h4></Link>
+                <Link to={'/add-agent'}><img src={AddIconOrangeSVG} width={12} alt={'Add icon'}/><h4>აგენტის
+                    დამატება</h4></Link>
 
             </div>
         </div>
 
         <div className={filterStyles['selectedFilters']}>
-
-
+            {filters.regionFilters.map((eachRegionFilter) =>
+                <div key={eachRegionFilter.id}
+                onClick={() => filterDispatch(removeRegionFilters(filters.regionFilters.filter(r =>  r.id !== eachRegionFilter.id)))}
+                >
+                    <p>{eachRegionFilter.name}</p>
+                    <img src={RemoveSVG} width={12} alt={'Remove icon'}/>
+                </div>
+            )}
+            {filtersAvailable && <button onClick={() => filterDispatch(resetAll({}))}>გასუფთავება</button>}
         </div>
     </section>
 }
