b59f7204fc67d52adf36063164f736526b16a4f0
Nick <nickgabriadze@gmail.com>
2024-09-14 20:16:09 +0400

    feat: implement agent card

---
+++ src/pages/listing/components/AgentCard.tsx
@@ -0,0 +1,30 @@
+import {Agent} from "../../../types/agents.ts";
+import agentStyles from '../styles/listingpage.agent.module.css'
+import PhoneSVG from '/src-icons/listing/phone.svg'
+import EmailSVG from '/src-icons/listing/email.svg'
+
+export default function AgentCard({agent}:{agent: Agent}){
+
+
+    return <div className={agentStyles['agentCardWrapper']}>
+        <div className={agentStyles['agentDetails']}>
+            <img src={agent.avatar}  alt="Agent picture"/>
+
+            <div>
+                <h5>{agent.name} {agent.surname}</h5>
+                <p>აგენტი</p>
+            </div>
+        </div>
+
+        <div className={agentStyles['contactDetails']}>
+            <div>
+                <img src={EmailSVG} alt={'Email icon'} />
+                <p>{agent.email}</p>
+            </div>
+            <div>
+                <img src={PhoneSVG} alt={'Phone icon'}/>
+                <p>{agent.phone.substring(0,3).concat(` ${agent.phone.substring(3, 6)}`).concat(` ${agent.phone.substring(6, 9)}`)}</p>
+            </div>
+        </div>
+    </div>
+}
