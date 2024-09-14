109610c18325a025e768af9ac8d51d1e80e26f9a
Nick <nickgabriadze@gmail.com>
2024-09-14 12:55:25 +0400

    feat: implement redux store provider for app

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import './global.css'
+import {Provider} from "react-redux";
+import {store} from "./store.ts";
 
 const queryClient = new QueryClient()
 
@@ -11,7 +13,9 @@ createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <App/>
+                <Provider store={store}>
+                    <App/>
+                </Provider>
             </BrowserRouter>
         </QueryClientProvider>
     </StrictMode>
