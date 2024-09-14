f0ffd3cc6f2f4db7193cb96067ec540026e52289
Nick <nickgabriadze@gmail.com>
2024-09-14 14:24:34 +0400

    feat: apply region filter to available listings

--- src/pages/listings-home/components/homes/Properties.tsx
+++ src/pages/listings-home/components/homes/Properties.tsx
@@ -2,6 +2,7 @@ import {useQuery} from "@tanstack/react-query";
 import getListings from "../../../../apis/getListings.ts";
 import Property from "./components/Property.tsx";
 import homeStyles from '../styles/homes.module.css';
+import {useAppSelector} from "../../../../hooks/redux.ts";
 
 
 export default function Properties() {
@@ -9,10 +10,12 @@ export default function Properties() {
         queryKey: ['homeListings'],
         queryFn: getListings
     })
+    const regionFilters = useAppSelector(s => s.filters.regionFilters).map((r) => r.id);
+
 
     const listings = homes.data?.data ? homes.data?.data : [];
 
-    return <section className={homeStyles['listingsWrapper']}>{listings?.map(
+    return <section className={homeStyles['listingsWrapper']}>{listings?.filter((l) => regionFilters.includes(l.city.region_id)).map(
         eachListing => <div key={eachListing.id}><Property home={eachListing}/></div>)
     }</section>
 
