2189ffe8a758cc3f0fa19afeb75b088a904ccad2
Nick <nickgabriadze@gmail.com>
2024-09-15 20:06:35 +0400

    feat: add function to reset agent info all together

--- src/features/forms/agentFormReducer.ts
+++ src/features/forms/agentFormReducer.ts
@@ -1,7 +1,7 @@
 import {Validation} from "../../types/validationOptions.ts";
 import {createSlice} from "@reduxjs/toolkit";
 
-type AgentFormState = { [key: string]: [string, Validation] }
+type AgentFormState = { [key: string]: [string | File, Validation] }
 
 
 const initialState: AgentFormState = {
@@ -17,7 +17,6 @@ const agentFormSlice = createSlice({
     initialState,
     reducers: {
         setAgentName: (state, action: { payload: [string, Validation] }) => {
-            console.log(action.payload)
             return {...state, name: action.payload}
         },
 
@@ -35,13 +34,23 @@ const agentFormSlice = createSlice({
 
         setAgentAvatar: (state, action: { payload: [string, Validation] }) => {
             return {...state, avatar: action.payload}
-        }
+        },
 
+        resetAgentInfo: (_, __) => {
+            return {
+                name: ['', false],
+                surname: ['', false],
+                email: ['', false],
+                phone: ['', false],
+                avatar: ['', false],
+            }
+        }
     }
 })
 export const {
     setAgentName, setAgentAvatar,
     setAgentSurname,
-    setAgentEmail, setAgentPhone
+    setAgentEmail, setAgentPhone,
+    resetAgentInfo
 } = agentFormSlice.actions;
 export default agentFormSlice.reducer
