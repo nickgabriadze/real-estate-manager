fd179c745e628e40edaac785121b00ac23fc6e8b
Nick <nickgabriadze@gmail.com>
2024-09-16 00:17:18 +0400

    refactor: update the custom select to use global state

--- src/global-components/Select.tsx
+++ src/global-components/Select.tsx
@@ -1,13 +1,20 @@
 import selectStyles from './styles/select.module.css'
+import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
+import {useAppDispatch} from "../hooks/redux.ts";
 
 
-export default function Select({data, label, selectOption}: { data: any[], label: string, selectOption: any }) {
-
-
+export default function Select({data, setValue, loading, label}: {
+    value: number,
+    setValue: ActionCreatorWithPayload<any>,
+    loading: boolean, data: any[], label: string
+}) {
+    const dispatch = useAppDispatch()
     return <div className={selectStyles['selectWrapper']}>
         <h5>{label}</h5>
-        <select name={label}
-                onChange={(e) => selectOption(parseInt(e.target.value))}>
+        <select name={label} disabled={loading}
+                onChange={(e) => {
+                    dispatch(setValue(parseInt(e.target.value)))
+                }}>
             {data.map((dataEntry) => <option value={dataEntry.id}
                                              key={dataEntry.id}>{dataEntry.name}</option>)}
         </select>
