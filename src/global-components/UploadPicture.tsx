400f3fff2450bc92fc289608dac8e05d1d5a61c1
Nick <nickgabriadze@gmail.com>
2024-09-15 20:05:10 +0400

    fix: update UploadPicture component to use global state

--- src/global-components/UploadPicture.tsx
+++ src/global-components/UploadPicture.tsx
@@ -1,28 +1,33 @@
 import uploadStyles from './styles/upload.module.css'
 import AddCircleSVG from '/src-icons/add-circle.svg'
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import DeleteImageSVG from '/src-icons/delete-bin.svg'
+import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
+import {useAppDispatch} from "../hooks/redux.ts";
 
-export default function UploadPicture({label, required}: { label: string, required: boolean }) {
-    const [file, setFile] = useState<File>()
-    const fileSize = Number(file?.size) / (1024 ** 2)
-    const [error, setError] = useState<boolean>(false)
+export default function UploadPicture({label, value, setValue, required}: {
+    label: string,
+    value: any,
+    setValue: ActionCreatorWithPayload<any>,
+    required: boolean
+}) {
+    const dispatch = useAppDispatch()
+    const fileSize = Number(value[0]?.size) / (1024 ** 2)
 
     useEffect(() => {
         if (!isNaN(fileSize)) {
             if (fileSize > 1) {
-                setError(true)
-                setFile(undefined)
+                dispatch(setValue(['', 'none']))
             } else {
-                setError(false)
+                dispatch(setValue([value[0], 'valid']))
             }
         }
 
-    }, [error, file]);
+    }, [value[1], value[0]]);
 
     const handleDeleteImage = () => {
         setTimeout(() => {
-            setFile(undefined);
+            dispatch(setValue(['', false]));
         }, 100);
     }
 
@@ -30,17 +35,17 @@ export default function UploadPicture({label, required}: { label: string, requir
         <h5>{label} {required ? "*" : ''}</h5>
 
         <label htmlFor={'pictureUpload'} className={uploadStyles['uploadContainer']}
-               style={error ? {border: '2px dotted var(--accent-color)'} : {}}
+               style={value[1] === 'none' ? {border: '2px dotted var(--accent-color)'} : {}}
         >
-            {file === undefined && <input type={'file'} id={'pictureUpload'}
+            {value[1] !== 'valid' && <input type={'file'} id={'pictureUpload'}
                                           accept={"image/png, image/jpg, image/jpeg"}
                                           onChange={(e) => {
                                               if (e.target.files !== null) {
-                                                  setFile(e.target.files[0])
+                                                  dispatch(setValue([(e.target.files[0]), 'valid']))
                                               }
                                           }}/>}
-            {!error && file ? <div className={uploadStyles['uploadedImage']}>
-                    <img src={!error && URL.createObjectURL(file)} alt={'Uploaded image'} draggable={false}/>
+            {value[1] === 'valid' && value[0] ? <div className={uploadStyles['uploadedImage']}>
+                    <img src={value[1] === 'valid' ? URL.createObjectURL(value[0]): ''} alt={'Uploaded image'} draggable={false}/>
                     <button
                         onClick={() => {
 
@@ -55,6 +60,6 @@ export default function UploadPicture({label, required}: { label: string, requir
                 </div> :
                 <img src={AddCircleSVG} alt={'Plus in a circle icon'} width={32}/>}
         </label>
-        {error && <p style={{color: 'var(--accent-color)'}}>ფაილის ზომა არ უნდა აღემატებოდეს 1 მეგაბაიტს</p>}
+        {value[1] === 'none' && <p style={{color: 'var(--accent-color)'}}>ფაილის ზომა არ უნდა აღემატებოდეს 1 მეგაბაიტს</p>}
     </div>
 }
