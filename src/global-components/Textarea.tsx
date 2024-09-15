82ea1a23222340a4ebd83fb18d05ce4b308af06a
Nick <nickgabriadze@gmail.com>
2024-09-15 15:31:14 +0400

    feat: customize textarea for new validation scheme

--- src/global-components/Textarea.tsx
+++ src/global-components/Textarea.tsx
@@ -1,15 +1,49 @@
 import textareaStyles from './styles/textarea.module.css'
 import CheckmarkSVG from "/src-icons/checkmark.svg";
+import {useAppDispatch} from "../hooks/redux.ts";
+import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
+import {Validation, ValidationOptions} from "../types/validationOptions.ts";
+import {validate} from "./validate.ts";
+import {useState} from "react";
+import CheckmarkErrorSVG from "/src-icons/checkmark-error.svg";
+import CheckmarkValidSVG from "/src-icons/checkmark-valid.svg";
 
 
-export default function Textarea({label, validator, required}: {label: string, validator: string, required: boolean}){
+export default function Textarea({label, validationType, value, setValue, validator, required}: {
+    label: string,
+    value: any,
+    validationType: ValidationOptions,
+    setValue: ActionCreatorWithPayload<any>,
+    validator: string,
+    required: boolean
+}) {
+    const [valid, setValid] = useState<Validation>('none')
+    const dispatch = useAppDispatch()
 
     return <div className={textareaStyles['textareaWrapper']}>
         <h5>{label} {required ? "*" : ''}</h5>
-        <textarea required={required}/>
+        <textarea
+            style={valid !== 'none' && !valid ? {
+                outline: '1px solid var(--accent-color)',
+                border: '1px solid var(--accent-color)'
+            } : valid === 'valid' ? {
+                outline: '1px solid var(--form-valid)',
+                border: '1px solid var(--form-valid)'
+            } : {}}
+            value={value[0]}
+            onChange={(e) => {
+                const valid = validate(validationType, String(e.target.value))
+                setValid(valid)
+                dispatch(setValue([e.target.value, valid]))
+
+            }}
+            required={required}/>
         {validator && <div>
-            <img src={CheckmarkSVG} alt={'Checkmark icon'} width={12}/>
-            <p>{validator}</p></div>}
+            <img
+                src={valid !== 'none' && !valid ? CheckmarkErrorSVG : valid === 'valid' ? CheckmarkValidSVG : CheckmarkSVG}
+                alt={'Checkmark icon'} width={12}/>
+            <p style={valid !== 'none' && !valid ? {color: 'var(--accent-color)'} : valid === 'valid' ? {color: 'var(--form-valid)'} : {}}>{validator}</p>
+        </div>}
 
     </div>
 }
