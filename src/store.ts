d7cee79825a3e5a8224f90865cfd6432d1fc0211
Nick <nickgabriadze@gmail.com>
2024-09-15 15:32:29 +0400

    refactor: add created reducers to store for global access

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filterReducer from "./features/filters/filterReducer.ts"
+import agentFormReducer from "./features/forms/agentFormReducer.ts";
+import listingFormReducer from "./features/forms/listingFormReducer..ts";
 
 export const store = configureStore({
     reducer: {
         filters: filterReducer,
+        listingForm: listingFormReducer,
+        agentForm: agentFormReducer
     },
 })
 
