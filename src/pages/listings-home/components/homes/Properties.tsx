dfcb802bd23db18bd05a7aaf84bd6fe8c512adf2
Nick <nickgabriadze@gmail.com>
2024-09-17 01:40:15 +0400

    feat: Properties are now filtered using area and price values

--- src/pages/listings-home/components/homes/Properties.tsx
+++ src/pages/listings-home/components/homes/Properties.tsx
@@ -16,6 +16,8 @@ export default function Properties() {
     })
     const regionFilters = useAppSelector(s => s.filters.regionFilters).map((r) => r.id);
     const roomFilters = useAppSelector(s => s.filters.roomFilters)
+    const areaFilters = useAppSelector(s => s.filters.areaFilter)
+    const priceFilters = useAppSelector(s => s.filters.priceFilter)
     const dispatch = useAppDispatch()
 
     const listings = homes.data?.data ? homes.data.data : []
@@ -30,7 +32,10 @@ export default function Properties() {
     const filters = useAppSelector(s => s.filters)
     const filtersAvailable = filters.regionFilters.length + filters.areaFilter.length + filters.priceFilter.length + filters.roomFilters.length !== 0
 
-    const filtered = listings?.filter((l) => regionFilters.includes(l.city.region_id) || roomFilters.includes(l.bedrooms))
+    const filtered = listings?.filter((l) => regionFilters.includes(l.city.region_id) || roomFilters.includes(l.bedrooms)
+    || (priceFilters.length === 2 && l.price > priceFilters[0] && l.price < priceFilters[1])
+        || (areaFilters.length === 2 && l.area > areaFilters[0] && l.area < areaFilters[1])
+    )
     return !homes.isLoading && filtered.length === 0 && filtersAvailable ? <p style={{'color': '#021526CC', fontWeight: '600'}}>აღნიშნული მონაცემებით განცხადება არ იძებნება</p> : <section
         className={homeStyles['listingsWrapper']}>{filtersAvailable ? filtered.map(
             eachListing => <div key={eachListing.id}><Property home={eachListing}/></div>) :
