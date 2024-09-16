606458ef782727141d491591ff25d67d0903e7d7
Nick <nickgabriadze@gmail.com>
2024-09-16 14:08:27 +0400

    style: update global font and set to FiraGO

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import './global.css'
+import '@fontsource/firago/300.css';
+import '@fontsource/firago/400.css';
+import '@fontsource/firago/500.css';
+import '@fontsource/firago/600.css';
+import '@fontsource/firago/700.css';
 import {Provider} from "react-redux";
 import {store} from "./store.ts";
 
