b8b4a77a868e1ed1f560c1f173097ac3e758756d
Nick <nickgabriadze@gmail.com>
2024-09-18 12:26:59 +0400

    refactor: add 'forAgent' value to indicate specific option for select

--- src/pages/create-listing/components/AgentDetails.tsx
+++ src/pages/create-listing/components/AgentDetails.tsx
@@ -2,9 +2,8 @@ import addListingStyles from "../addlisting.module.css";
 import {useQuery} from "@tanstack/react-query";
 import getAgents from "../../../apis/agent/getAgents.ts";
 import Select from "../../../global-components/Select.tsx";
-import {useAppDispatch, useAppSelector} from "../../../hooks/redux.ts";
+import {useAppSelector} from "../../../hooks/redux.ts";
 import {setAgentId} from "../../../features/forms/listingFormReducer.ts";
-import {useEffect} from "react";
 
 export default function AgentDetails() {
     const {agent_id} = useAppSelector(s => s.listingForm)
@@ -18,12 +17,8 @@ export default function AgentDetails() {
             name: a.name.concat(` ${a.surname}`)
         }
     }) : []
-    const dispatch = useAppDispatch()
-    useEffect(() => {
-        if (!isLoading) {
-            dispatch(setAgentId(Number(agents[0].id)))
-        }
-    }, [isLoading]);
+
+
 
     return <div className={addListingStyles['agents']}>
         <h3>აგენტი</h3>
@@ -31,6 +26,7 @@ export default function AgentDetails() {
             value={agent_id}
             setValue={setAgentId}
             loading={isLoading}
+            forAgent={true}
             data={isLoading ? [{name: 'აგენტი', id: -1}] : agents} label={'აირჩიე'}/>
 
     </div>
