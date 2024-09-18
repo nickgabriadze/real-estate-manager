54e07321e8545b230fa53d8d8dfdcbdeaf580f73
Nick <nickgabriadze@gmail.com>
2024-09-18 12:26:23 +0400

    feat: implement custom designed select component with option to add agent included in select options

--- src/global-components/Select.tsx
+++ src/global-components/Select.tsx
@@ -1,22 +1,47 @@
+import AddCircleSVG from "/src-icons/add-round-circle.svg";
 import selectStyles from './styles/select.module.css'
 import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
 import {useAppDispatch} from "../hooks/redux.ts";
+import {useState} from "react";
+import useClickOutside from "../hooks/useClickOutside.ts";
+import {Link} from "react-router-dom";
 
 
-export default function Select({data, setValue, loading, label}: {
+export default function Select({data, value, setValue, loading, label, forAgent}: {
     value: number,
+    forAgent?: boolean
     setValue: ActionCreatorWithPayload<any>,
     loading: boolean, data: any[], label: string
 }) {
+    const [dropDownOpen, setDropDownOpen] = useState<boolean>(false)
+    const selectClickOutside = useClickOutside(() => setDropDownOpen(false))
     const dispatch = useAppDispatch()
+    const selectedValue = data.find(item => item.id === value) === undefined ? 'აირჩიე' : data.find(item => item.id === value).name
+
+
     return <div className={selectStyles['selectWrapper']}>
         <h5>{label}</h5>
-        <select name={label} disabled={loading}
-                onChange={(e) => {
-                    dispatch(setValue(parseInt(e.target.value)))
-                }}>
-            {data.map((dataEntry) => <option value={dataEntry.id}
-                                             key={dataEntry.id}>{dataEntry.name}</option>)}
-        </select>
+
+        <div className={selectStyles['dropDownWrapper']} ref={selectClickOutside}
+             onClick={() => setDropDownOpen(prev => !prev)}>
+            <div className={`${selectStyles['dropDownMainOption']} ${dropDownOpen && selectStyles['dropDownOpen']}`}
+
+            >{loading ? 'აირჩიე' : value === -1 ? 'აირჩიე' : selectedValue}
+            </div>
+            {dropDownOpen && <div className={selectStyles['dropDownOptions']}
+                                  style={data.length > 5 ? {height: '200px'} : {height: 'fit-content'}}
+            >
+                {forAgent && <p className={`${selectStyles['dropDownOption']} ${selectStyles['addAgentOption']}`}>
+                    <img  src={AddCircleSVG} width={24} alt={'Add icon'}/>
+                    <Link
+                    to={'/add-agent'}
+                >დაამატე აგენტი</Link></p>}
+
+                {data.map((dataEntry) => <p
+                    className={selectStyles['dropDownOption']}
+                    onClick={() => dispatch(setValue(parseInt(dataEntry.id)))}
+                    key={dataEntry.id}>{dataEntry.name}</p>)}
+            </div>}
+        </div>
     </div>
 }
