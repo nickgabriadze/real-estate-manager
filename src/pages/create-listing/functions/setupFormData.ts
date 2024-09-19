2820d4ac16e12a5c32edd812050a36fd1f904e2e
Nick <nickgabriadze@gmail.com>
2024-09-19 22:47:12 +0400

    fix: listing form data was not setup properly

--- src/pages/create-listing/functions/setupFormData.ts
+++ src/pages/create-listing/functions/setupFormData.ts
@@ -7,13 +7,13 @@ export default function setupFormData(listingForm: ListingForm){
     formData.append('is_rental', String(listingForm.is_rental))
     formData.append('address', listingForm.address[0])
     formData.append('zip_code', listingForm.zip_code[0])
-    formData.append('region_id', String(listingForm.region))
-    formData.append('city_id', String(listingForm.city))
+    formData.append('region_id', String(listingForm.region[0]))
+    formData.append('city_id', String(listingForm.city[0]))
     formData.append('price', String(listingForm.price[0]))
     formData.append('area', String(listingForm.area[0]))
     formData.append('bedrooms', String(listingForm.bedrooms[0]))
     formData.append('description', String(listingForm.description[0]))
-    formData.append('agent_id', String(listingForm.agent_id))
+    formData.append('agent_id', String(listingForm.agent_id[0]))
     formData.append('image', listingForm.image[0])
 
     return formData
