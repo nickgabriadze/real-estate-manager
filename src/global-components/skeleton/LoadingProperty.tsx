77cf8f948f048eebdd53fc6774e76cf8b31b4363
Nick <nickgabriadze@gmail.com>
2024-09-19 20:48:07 +0400

    feat: implement loading skeleton for Listing property component

---
+++ src/global-components/skeleton/LoadingProperty.tsx
@@ -0,0 +1,23 @@
+import propertyLoadingStyles from '../styles/skeleton/loading.property.module.css'
+
+
+export function LoadingProperty(){
+
+
+    return <div className={propertyLoadingStyles['loadingPropertySkeletonWrapper']}>
+            <div className={propertyLoadingStyles['imageWrapper']}>
+                <div className={propertyLoadingStyles['rentalSkeleton']}></div>
+                <div className={propertyLoadingStyles['imageSkeleton']}></div>
+            </div>
+
+        <div className={propertyLoadingStyles['detailsWrapper']}>
+            <div className={propertyLoadingStyles['priceSkeleton']}></div>
+            <div className={propertyLoadingStyles['locationSkeleton']}></div>
+            <div className={propertyLoadingStyles['detailsSkeleton']}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+}
--- src/pages/listings-home/components/homes/Properties.tsx
+++ src/pages/listings-home/components/homes/Properties.tsx
@@ -6,6 +6,7 @@ import {useAppDispatch, useAppSelector} from "../../../../hooks/redux.ts";
 import {setTotalAvailableRooms} from "../../../../features/filters/filterReducer.ts";
 import {useEffect} from "react";
 import {addListingsBulk} from "../../../../features/listings/listingReducer.ts";
+import {LoadingProperty} from "../../../../global-components/skeleton/LoadingProperty.tsx";
 
 
 export default function Properties() {
@@ -23,25 +24,31 @@ export default function Properties() {
     const listings = homes.data?.data ? homes.data.data : []
 
     useEffect(() => {
-            if(!homes.isLoading && homes.data?.data) {
-                dispatch(setTotalAvailableRooms(Array.from(new Set(homes.data.data.map(l => l.bedrooms)))))
-                dispatch(addListingsBulk(homes.data.data))
-            }
+        if (!homes.isLoading && homes.data?.data) {
+            dispatch(setTotalAvailableRooms(Array.from(new Set(homes.data.data.map(l => l.bedrooms)))))
+            dispatch(addListingsBulk(homes.data.data))
+        }
     }, [homes.isLoading]);
 
     const filters = useAppSelector(s => s.filters)
     const filtersAvailable = filters.regionFilters.length + filters.areaFilter.length + filters.priceFilter.length + filters.roomFilters.length !== 0
 
     const filtered = listings?.filter((l) => regionFilters.includes(l.city.region_id) || roomFilters.includes(l.bedrooms)
-    || (priceFilters.length === 2 && l.price > priceFilters[0] && l.price < priceFilters[1])
+        || (priceFilters.length === 2 && l.price > priceFilters[0] && l.price < priceFilters[1])
         || (areaFilters.length === 2 && l.area > areaFilters[0] && l.area < areaFilters[1])
     )
-    return !homes.isLoading && filtered.length === 0 && filtersAvailable ? <p style={{'color': '#021526CC', fontWeight: '600'}}>აღნიშნული მონაცემებით განცხადება არ იძებნება</p> : <section
-        className={homeStyles['listingsWrapper']}>{filtersAvailable ? filtered.map(
-            eachListing => <div key={eachListing.id}><Property home={eachListing}/></div>) :
-        listings.map(
-            eachListing => <div key={eachListing.id}><Property home={eachListing}/></div>
-        )
-    }</section>
+
+
+    return homes.isLoading ?
+        <div className={homeStyles['listingsWrapper']}>{Array.from({length: 30}).map((_, i) => <LoadingProperty
+            key={i}/>)}</div> : filtered.length === 0 && filtersAvailable ?
+            <p style={{'color': '#021526CC', fontWeight: '600'}}>აღნიშნული მონაცემებით განცხადება არ იძებნება</p> :
+            <section
+                className={homeStyles['listingsWrapper']}>{filtersAvailable ? filtered.map(
+                    eachListing => <div key={eachListing.id}><Property home={eachListing}/></div>) :
+                listings.map(
+                    eachListing => <div key={eachListing.id}><Property home={eachListing}/></div>
+                )
+            }</section>
 
 }
