5212c3674411e972dd00e02c873b8b8dc6c066a7
Nick <nickgabriadze@gmail.com>
2024-09-20 19:08:26 +0400

    feat: add carousel navigation for other listings

--- src/pages/listing/components/OtherListings.tsx
+++ src/pages/listing/components/OtherListings.tsx
@@ -17,7 +17,6 @@ export default function OtherListings({region_id}: { region_id: number }) {
     const dispatch = useAppDispatch()
     const windowWidth = useWindowWidth()
     const incrementor = Math.max(1, (Math.floor((windowWidth * 0.8) / 350)))
-
     const [carouselIndex, setCarouselIndex] = useState<[number, number]>([0, incrementor])
     const {data, isLoading} = useQuery({
         queryKey: ['realEstate', listings.length],
@@ -28,48 +27,75 @@ export default function OtherListings({region_id}: { region_id: number }) {
         if (listings.length === 0 && !isLoading && data?.data) {
             dispatch(addListingsBulk(data?.data))
         }
-    }, [listings.length, isLoading]);
+    }, [listings.length, isLoading, region_id, id]);
 
     useEffect(() => {
         setCarouselIndex(() => [0, incrementor])
     }, [incrementor, id]);
+    const carouselListings = (incDec: 0 | 1) => {
+        if (incDec === 1) {
+            if (carouselIndex[1] + 1 <= similarListings.length) {
+                setCarouselIndex([carouselIndex[0] + 1, carouselIndex[1] + 1])
+            } else {
+                setCarouselIndex(prev => [prev[0] + 1, 0])
+            }
+
+            if (carouselIndex[0] + 1 > similarListings.length) {
+                setCarouselIndex(prev => [0, prev[1]])
+            }
+        }
+        if (incDec === 0) {
+            if (carouselIndex[0] - 1 >= 0) {
+                setCarouselIndex([carouselIndex[0] - 1, carouselIndex[1] - 1])
+            } else {
+                setCarouselIndex(prev => [similarListings.length, prev[1] - 1])
+            }
+
+            if (carouselIndex[1] - 1 < 0) {
+                setCarouselIndex(prev => [prev[0], similarListings.length])
+
+            }
+        }
+    }
+
+    console.log(carouselIndex)
+    const handleCarousel = () => {
+        const [startIndex, endIndex] = carouselIndex
+        const wrapperDiff = incrementor - 1 === startIndex - endIndex
+
+        if (startIndex <= endIndex) {
+            if (wrapperDiff) {
+                return similarListings.slice(startIndex, endIndex - 1)
+            } else {
+                return similarListings.slice(startIndex, endIndex)
+            }
+
+        } else {
+            if (wrapperDiff) {
+                return similarListings.slice(endIndex, startIndex + 1)
+            } else {
+                return similarListings.slice(endIndex, startIndex)
+            }
+        }
+    }
 
     return similarListings.length > 0 && <div className={similarListingsStyle['othersWrapper']}>
         <h3>ბინები მსგავს ლოკაციაზე</h3>
 
         <div className={similarListingsStyle['propertiesWrapper']}>
             <button
-                className={`${similarListingsStyle['leftArrow']} ${carouselIndex[0] === 0 && similarListingsStyle['arrowDisabled']}`}
-                onClick={() => {
-                    if (carouselIndex[0] === 0) return;
-                    else {
-                        if (carouselIndex[0] - incrementor >= 0) {
-                            setCarouselIndex((prev) => [prev[0] - incrementor, prev[0]])
-                        } else {
-
-                            setCarouselIndex([0, incrementor]);
-                        }
-                    }
-                }}
+                className={`${similarListingsStyle['leftArrow']} ${similarListings.length <= incrementor && similarListingsStyle['arrowDisabled']}`}
+                onClick={() => similarListings.length > incrementor && carouselListings(0)}
             ><img src={LeftArrowSVG} width={30} alt={'Left arrow icon'}/></button>
-            <div className={similarListingsStyle['list']} style={{gridTemplateColumns: `repeat(${incrementor}, minmax(380px, 1fr))`}}>
-                {similarListings.slice(carouselIndex[0], carouselIndex[1]).map(eachListing => <Property
+            <div className={similarListingsStyle['list']}
+                 style={{gridTemplateColumns: `repeat(${incrementor}, minmax(${windowWidth * 0.8 / 350}px, 1fr))`}}>
+                {handleCarousel().map(eachListing => <Property
                     key={eachListing.id}
                     home={eachListing}/>)}
             </div>
             <button
-                className={`${similarListingsStyle['rightArrow']} ${carouselIndex[1] >= similarListings.length && similarListingsStyle['arrowDisabled']}`}
-                onClick={() => {
-                    if (carouselIndex[1] >= similarListings.length) return;
-                    else {
-                        if (carouselIndex[1] + incrementor <= similarListings.length) {
-                            setCarouselIndex((prev) => [prev[1], prev[1] + incrementor])
-                        } else {
-                            const diff = similarListings.length - carouselIndex[1]
-                            setCarouselIndex(prev => [prev[1], prev[1] + diff])
-                        }
-                    }
-                }}
+                className={`${similarListingsStyle['rightArrow']}  ${similarListings.length <= incrementor && similarListingsStyle['arrowDisabled']}`}
+                onClick={() => similarListings.length > incrementor && carouselListings(1)}
             ><img src={LeftArrowSVG} width={30} alt={'Right arrow icon'}/></button>
 
         </div>
