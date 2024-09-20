deba14eea231133fd3e7182a76d3abc743296b3d
Nick <nickgabriadze@gmail.com>
2024-09-20 22:54:42 +0400

    fix: carousel wasn't working correctly

--- src/pages/listing/components/OtherListings.tsx
+++ src/pages/listing/components/OtherListings.tsx
@@ -17,7 +17,7 @@ export default function OtherListings({region_id}: { region_id: number }) {
     const dispatch = useAppDispatch()
     const windowWidth = useWindowWidth()
     const incrementor = Math.max(1, (Math.floor((windowWidth * 0.8) / 350)))
-    const [carouselIndex, setCarouselIndex] = useState<[number, number]>([0, incrementor])
+    const [carouselIndex, setCarouselIndex] = useState<[number, number]>([0, incrementor - 1])
     const {data, isLoading} = useQuery({
         queryKey: ['realEstate', listings.length],
         queryFn: () => getListings(),
@@ -30,52 +30,44 @@ export default function OtherListings({region_id}: { region_id: number }) {
     }, [listings.length, isLoading, region_id, id]);
 
     useEffect(() => {
-        setCarouselIndex(() => [0, incrementor])
+        setCarouselIndex(() => [0, incrementor - 1])
     }, [incrementor, id]);
     const carouselListings = (incDec: 0 | 1) => {
         if (incDec === 1) {
-            if (carouselIndex[1] + 1 <= similarListings.length) {
-                setCarouselIndex([carouselIndex[0] + 1, carouselIndex[1] + 1])
+            if (carouselIndex[1] + 1 < similarListings.length) {
+                if (carouselIndex[0] + 1 >= similarListings.length) {
+                    setCarouselIndex(prev => [0, prev[1] + 1])
+                } else {
+                    setCarouselIndex([carouselIndex[0] + 1, carouselIndex[1] + 1])
+                }
             } else {
                 setCarouselIndex(prev => [prev[0] + 1, 0])
             }
 
-            if (carouselIndex[0] + 1 > similarListings.length) {
-                setCarouselIndex(prev => [0, prev[1]])
-            }
+
         }
         if (incDec === 0) {
             if (carouselIndex[0] - 1 >= 0) {
-                setCarouselIndex([carouselIndex[0] - 1, carouselIndex[1] - 1])
+                if (carouselIndex[1] - 1 < 0) {
+                    setCarouselIndex(prev => [prev[0] - 1, similarListings.length-1])
+                } else {
+                    setCarouselIndex([carouselIndex[0] - 1, carouselIndex[1] - 1])
+                }
             } else {
-                setCarouselIndex(prev => [similarListings.length, prev[1] - 1])
+                setCarouselIndex(prev => [similarListings.length - 1, prev[1] - 1])
             }
 
-            if (carouselIndex[1] - 1 < 0) {
-                setCarouselIndex(prev => [prev[0], similarListings.length])
 
-            }
         }
     }
 
-    console.log(carouselIndex)
     const handleCarousel = () => {
         const [startIndex, endIndex] = carouselIndex
-        const wrapperDiff = incrementor - 1 === startIndex - endIndex
-
         if (startIndex <= endIndex) {
-            if (wrapperDiff) {
-                return similarListings.slice(startIndex, endIndex - 1)
-            } else {
-                return similarListings.slice(startIndex, endIndex)
-            }
+            return similarListings.slice(startIndex, endIndex + 1)
 
         } else {
-            if (wrapperDiff) {
-                return similarListings.slice(endIndex, startIndex + 1)
-            } else {
-                return similarListings.slice(endIndex, startIndex)
-            }
+            return similarListings.slice(endIndex, startIndex + 1)
         }
     }
 
